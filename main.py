c64656991e4268073a78fe6ff003cc41bb1c0fab
Aliff Abedin <aliffahnaff2020@gmail.com>
2026-01-04 12:56:26 +0000

    Add files via upload
    Accuracy seems really off but I'm not really sure on how to tweak parameters to make it more accurate (also I'm aware that accuracy isn't the best in terms of models concerning medical datasets)
    Used logistic regression because it allowed me to get outputs which were represented through probability which was my aim in making this

---
+++ main.py
@@ -0,0 +1,34 @@
+from firstdataset import *
+from seconddataset import * 
+import matplotlib.pyplot as plt
+zvalues1 = firstdataset()
+zvalues2 = seconddataset()
+
+person = 0
+ypoints = []
+xpoints = []
+
+for z1 , z2 in zip(zvalues1 , zvalues2):
+    person += 1
+    combinedz = 0.6 * z1 + 0.4 * z2
+    xpoints.append(combinedz)
+    risk = 1 / (1 + np.exp(-combinedz))
+    riskpercent = round(risk * 100)
+    ypoints.append(riskpercent)
+    print(f"Person {person}'s risk of cancer: {riskpercent}%")
+
+
+#set limits based on risk percentage amd combined z
+#make the graph look better
+plt.xlim(min(xpoints) , max(xpoints))
+ypad = 0.05 * max(ypoints)
+plt.ylim(0,max(ypoints) + ypad)
+plt.scatter(xpoints, ypoints, alpha=0.6)
+
+plt.title("Cancer risk Assessment")
+plt.ylabel("Predicted risk of Cancer")
+plt.xlabel("overall risk score of all features (clinical + environmental + symptomatic)")
+plt.show()
+
+
+
